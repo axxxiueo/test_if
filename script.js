27bb199e4171e14714eb50622203eb8e5f11faaa
axxxiueo <77669299+axxxiueo@users.noreply.github.com>
2023-10-01 09:49:42 +0900

    modify

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const weather = ["晴れ","曇り","雨"];
-for (let i = 0; i < weather.length; i++){
+// 配列の数に対して自動で繰り返す
+
+const weather = ["晴れ","曇り","雨","雷"]
+for (let i = 0; i < weather.length ; i++){
     let li = document.createElement("li");
     li.textContent = weather[i];
-    document.getElementsByTagName("number").appendeChild(li);
+    document.getElementById("number").appendChild(li);
 }
